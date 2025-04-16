cbddcc3fd70f7922847e09a58116706e57c99a17
jacob-rubin <jacob.rubin18@gmail.com>
2025-04-16 16:32:00 -0500

    feat: add template for login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-4 p-2">
+	<input type="text" placeholder="Login" class="input" />
+	<input type="text" placeholder="Password" class="input" />
+</div>
