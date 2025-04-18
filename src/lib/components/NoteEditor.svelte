84e1715805dd30ae2af2791c8c1d1afd01e3deaa
jacob-rubin <jacob.rubin18@gmail.com>
2025-04-18 15:20:55 -0500

    add delete button

--- src/lib/components/NoteEditor.svelte
+++ src/lib/components/NoteEditor.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
-	import { type Note } from '../../store/noteState.svelte';
+	import { noteManager, type Note } from '../../store/noteState.svelte';
+	import TablerTrash from '~icons/tabler/trash';
 
 	interface Props {
 		note: Note;
 	}
 
 	let { note }: Props = $props();
+
+	const deleteNote = () => {
+		noteManager.deleteNote(note.id);
+	};
 </script>
 
-<div class={`card card-sm card-border w-full shadow-xs`}>
-	<div class="card-body">
-		<input
-			type="text"
-			class="input input-bordered card-title w-full"
-			bind:value={note.title}
-			placeholder="Title"
-		/>
-		<textarea class="textarea w-full" placeholder="Note" bind:value={note.content}></textarea>
+<div class="flex flex-row items-center justify-between gap-2">
+	<div class={`card card-sm card-border w-full shadow-xs`}>
+		<div class="card-body">
+			<input
+				type="text"
+				class="input input-bordered card-title w-full"
+				bind:value={note.title}
+				placeholder="Title"
+			/>
+			<textarea class="textarea w-full" placeholder="Note" bind:value={note.content}></textarea>
+		</div>
 	</div>
+	<button class="btn" onclick={deleteNote}>
+		<TablerTrash />
+	</button>
 </div>
